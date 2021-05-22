1729ab3ba2da3a873aed0be5cc7befcc97100d95
Joe Sylnice <nerdroid23@gmail.com>
2021-05-22 09:22:52 -0400

    initial commit

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Nerdroid23\BladeIcomoon\Tests;
 
 class ExampleTest extends TestCase
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,36 +1,27 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Nerdroid23\BladeIcomoon\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use VendorName\Skeleton\SkeletonServiceProvider;
+use Nerdroid23\BladeIcomoon\BladeIcomoonServiceProvider;
 
 class TestCase extends Orchestra
 {
     public function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            SkeletonServiceProvider::class,
+            BladeIcomoonServiceProvider::class,
         ];
     }
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('database.default', 'testing');
-
-        /*
-        include_once __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
-        (new \CreatePackageTable())->up();
-        */
+        //
     }
 }
